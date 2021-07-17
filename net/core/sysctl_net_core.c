318769546a6a74125db723a8baaacfaa7deaa1fe
madaidan <50278627+madaidan@users.noreply.github.com>
2021-07-17 15:39:25 +0000

    Make more sysctl constants read-only

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -22,13 +22,13 @@
 #include <net/busy_poll.h>
 #include <net/pkt_sched.h>
 
-static int two = 2;
-static int three = 3;
-static int min_sndbuf = SOCK_MIN_SNDBUF;
-static int min_rcvbuf = SOCK_MIN_RCVBUF;
-static int max_skb_frags = MAX_SKB_FRAGS;
-static long long_one __maybe_unused = 1;
-static long long_max __maybe_unused = LONG_MAX;
+static int two __read_only = 2;
+static int three __read_only = 3;
+static int min_sndbuf __read_only = SOCK_MIN_SNDBUF;
+static int min_rcvbuf __read_only = SOCK_MIN_RCVBUF;
+static int max_skb_frags __read_only = MAX_SKB_FRAGS;
+static long long_one __maybe_unused __read_only = 1;
+static long long_max __maybe_unused __read_only = LONG_MAX;
 
 static int net_msg_warn;	/* Unused, but still a sysctl */
 
